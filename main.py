14394e0e9da209f97cfef8f050c6efe6a4dfa253
Dikkurik <oimatetb@gmail.com>
2024-06-23 14:14:53 +0300

    initial commit, created basic CRUD-operations

---
+++ main.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.methods import ForwardMessages
+from aiogram.types import Message, MessageOriginChannel
+
+from note_editor import config, Note
+
+# Bot token can be obtained via https://t.me/BotFather
+# TOKEN = getenv()
+TOKEN = config["BOT"]["TOKEN"]
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+# bot methods
+
+@dp.message(Command(prefix="/",commands="help"))
+async def get_files(message: Message):
+    """
+    This command return help message 
+    """
+    help_msg = """This is bot for CRUD operations with notes on PC through telegram,
+created for fun.
+/nget - get all notes in folder
+/nread [filename] - read note
+/nedit [filename] - edit note"""
+
+    await message.answer(text=f"{help_msg}")
+
+@dp.message(Command(prefix="/",commands="nget"))
+async def get_files(message: Message):
+    """
+    This command return list of existed files 
+    """
+    note = Note()
+    n_list = note.getNotes()
+    await message.answer(text=f"Existed files:\n{n_list}")
+
+@dp.message(Command(prefix="/",commands="nread"))
+async def read_note(message: Message):
+    """
+    This command return list of existed files 
+    """
+    note = Note()
+    n_text = note.readNote(message.text.split(' ')[1])
+    await message.answer(text=f"{n_text}")
+
+@dp.message(Command(prefix="/",commands="nedit"))
+async def edit_note(message: Message):
+    """
+    This command return list of existed files 
+    """
+    note = Note()
+    n_text = note.readNote(message.text.split(' ')[1])
+    print(n_text)
+    await message.answer(text=f"{n_text}")
+
+# channel methods
+
+@dp.channel_post(Command(prefix="/",commands="help"))
+async def get_files(message: Message):
+    """
+    This command return help message 
+    """
+    help_msg = """This is bot for CRUD operations with notes on PC through telegram,
+created for fun.
+/nget - get all notes in folder
+/nread [filename] - read note
+/nedit [filename] - edit note"""
+
+    await message.answer(text=f"{help_msg}")
+
+@dp.channel_post(Command(prefix="/",commands="nget"))
+async def get_files(message: Message):
+    """
+    This command return list of existed files 
+    """
+    note = Note()
+    n_list = note.getNotes()
+    await message.answer(text=f"Existed files:\n{n_list}")
+
+@dp.channel_post(Command(prefix="/",commands="nread"))
+async def read_note(message: Message):
+    """
+    This command return list of existed files 
+    """
+    note = Note()
+    n_text = note.readNote(message.text.split(' ')[1])
+    await message.answer(text=f"{n_text}")    
+
+@dp.channel_post(Command(prefix="/",commands="nedit"))
+async def edit_note(message: Message):
+    """
+    This command return list of existed files 
+    """
+    note = Note()
+    n_text = note.editNote(message.text.split(" ")[1], message.text.split("  ")[1])
+    print(n_text)
+    await message.answer(text=f"{n_text}")
+
+@dp.channel_post(Command(prefix="/",commands="clear"))
+async def read_note(message: Message):
+    """
+    This command clear chanel  
+    """
+    message.delete
+    await message.answer()
+
+@dp.channel_post()
+async def create_note(message: Message):
+    try:
+        note = Note()
+        note.createNote(message.text)
+        note.saveToFile()
+        print("saved to yourfile")
+        await message.delete()
+    except:
+        print("Something went wrong")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
---
+++ note_editor.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import configparser, os
+
+config = configparser.ConfigParser()
+config.read("config.cfg", encoding="UTF-8")
+
+
+class Note():
+    def __init__(self, ):
+        """
+        Define notes folder and extension for file.
+        """
+        self._path_to_folder = config["FOLDERS"]["NOTE_FOLDER"]
+        self._ext = config["FOLDERS"]["FILE_EXT"]
+
+    def createNote(self, message: str):
+        """
+        When this metod called it create file with name
+        """
+        self.name = message.split('\n')[0]
+        self.note = message.removeprefix(f"{self.name}\n")
+        
+    def saveToFile(self,):
+        """
+        Saving file to folder defined in config.cfg
+        """
+        with open(f"{self._path_to_folder}\{self.name}.{self._ext}", "w", encoding="UTF-8") as note_file:
+            note_file.write(self.note)
+        print(f"Note saved to {self._path_to_folder}")
+
+    def getNotes(self):
+        """
+        Return list of all files in folder
+        """
+        listfile = os.listdir(self._path_to_folder)
+        note_list = ""
+        for file in listfile:
+            note_list=note_list+f"{file}\n"
+        return note_list
+    
+    def readNote(self, filename):
+        with open(f"{self._path_to_folder}\{filename}.{self._ext}", "r", encoding="UTF-8") as note:
+            note_text = note.read()
+        print(note_text)
+        return note_text
+
+    def editNote(self, filename: str, text: str):
+        with open(f"{self._path_to_folder}\{filename}.{self._ext}", "r", encoding="UTF-8") as note:
+            note_text = note.read()
+        note_text = note_text+f"{text}\n"
+
+        with open(f"{self._path_to_folder}\{filename}.{self._ext}", "w", encoding="UTF-8") as note:
+            note.write(note_text)
+
+        return note_text
+
+    
+
+
+
+
